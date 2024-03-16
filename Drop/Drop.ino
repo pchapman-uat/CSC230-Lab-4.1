0d30ec5b098d4c77ee3fd59f8e794a5b5eec58c4
Preston Chapman <pchapman82070@uat.edu>
2024-03-16 16:22:11 -0700

    Update Drop Code

--- Drop/Drop.ino
+++ Drop/Drop.ino
@@ -1,11 +1,16 @@
 #include <M5StickCPlus.h>
 
+#define LED_BUILTIN 10
 
 float accX = 0.0f;
 float accY = 0.0f;
 float accZ = 0.0f;
 
 float maxZ = 0.0f;
+
+
+bool isDrop = false;
+float blinkCount = 0;
 void setup()
 {
 	M5.begin();
@@ -15,10 +20,14 @@ void setup()
     M5.Lcd.setTextSize(1);
     M5.Lcd.setCursor(80, 15); 
     M5.Lcd.println("DROP TEST");
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop()
 {
+
 	M5.IMU.getAccelData(&accX, &accY, &accZ);
     if(accZ > maxZ) maxZ = accZ;
     M5.Lcd.setTextSize(1);
@@ -29,4 +38,22 @@ void loop()
     M5.Lcd.setCursor(30, 80);
     M5.Lcd.printf(" %5.2f", maxZ);
 
+    M5.update();
+    if(m5.BtnA.wasReleased()){
+        maxZ = 0.0f;
+        isDrop = false;
+        M5.Lcd.fillScreen(BLACK);
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    if(maxZ >= 5.0f && isDrop == false){
+        M5.Lcd.fillScreen(RED);
+        isDrop = true;
+    }
+
+    if(isDrop) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(200);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
